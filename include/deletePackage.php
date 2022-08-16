7704e5e8faa6ea91ab562592bda5d8ce486234a8
npoudelp <npoudelp@gmail.com>
2022-08-16 07:51:43 +0545

    admin package deletion depoyed

--- include/deletePackage.php
+++ include/deletePackage.php
@@ -6,7 +6,6 @@ if ($_SESSION['logged'] != 'true') {
 if (isset($_REQUEST['q'])) {
     $packId = $_REQUEST['q'];
     $image = $_REQUEST['r'];
-    echo $image;
     include_once("./dbConn.php");
 
     $sqlImage = "SELECT image FROM package WHERE pid = '{$packId}';";
@@ -14,17 +13,17 @@ if (isset($_REQUEST['q'])) {
     if (mysqli_num_rows($getImage) > 0) {
         while ($row = mysqli_fetch_assoc($getImage)) {
             $image = $row['image'];
-            if (unlink($image)) {
-                $sql = "DELETE FROM package WHERE pid = '{$packId}';";
+    if (unlink($image)) {
+    $sql = "DELETE FROM package WHERE pid = '{$packId}';";
 
-                $result = mysqli_query($conn, $sql);
-                if ($result) {
-                    header("location: ../pages/adminPackage.php?msg=success");
-                }
-            }else{
-                header("location: ../pages/adminPackage.php?msg=failed");
-            }
-        }
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header("location: ../pages/adminPackage.php?msg=success");
+    }
+    } else {
+        header("location: ../pages/adminPackage.php?msg=failed");
+    }    
+    }
     }
 } else {
     header("location:../index.php");
