daa539eb4b20b25b909903ac0ae9d83ef6a59165
npoudelp <npoudelp@gmail.com>
2022-08-16 07:52:54 +0545

    basic typo errors

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+$msg = "";
+if (isset($_REQUEST['msg'])) {
+    $msg = $_REQUEST['msg'];
+}
+
+include_once('./include/dbConn.php');
+$sql = "SELECT * FROM review WHERE status=1;";
+
+$res = mysqli_query($conn, $sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -50,17 +62,97 @@
     <!-- Navbar ends here -->
 
     <!-- Reviews starts here -->
-    <section class="p-5 lead">
+    <section class="lead py-3">
         <div class="container">
-            Here review will appear
+            <div class="row py-3">
+                <div class="col-md-6 py-1 shadow">
+                    <?php
+                    if (mysqli_num_rows($res)) {
+                        while ($row = mysqli_fetch_assoc($res)) {
+
+
+
+                    ?>
+                            <div class="row">
+                                <div class="col-3">
+                                    <img src="./images/<?php echo $row['image']; ?>" class="rounded-circle" height="80px" width="80px">
+                                </div>
+                                <div class="col-9">
+                                    <div class="row">
+                                        <span class=""><i class="bi bi-quote text-muted"></i>
+                                            <?php echo $row['review']; ?>
+                                        </span>
+                                        <div class="row">
+                                            <p class="text-muted">-
+                                                <?php echo $row['name']; ?>
+                                                ,
+                                                <?php echo $row['address']; ?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php
+                        }
+                    } else {
+                        ?>
+                        <span class="text-danger lead">No reviews available</span>
+                    <?php
+
+
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
     </section>
     <!-- Reviews ends here -->
 
 
-    <?php
-    include_once('./include/footer.php');
-    ?>
+    <section class="bg-dark text-white lead">
+        <div class="container-flex mx-5">
+            <div class="row">
+                <div class="col-lg p-3">
+                    <i class="bi bi-geo-alt-fill h3 text-danger"></i> &nbsp; <span class="h3 text-light">Find Us</span>
+                    <p class="border-top border-secondary py-1">
+                        SCF 1, Kalka Shimla National Highway Road, Wadhawa Nagar Hotal NEO Classic Backside Dhakoli, Zirakpur, Chandigarh
+                    </p><br>
+                    <p><i class="bi bi-telephone-fill  text-warning"> </i>+91-9815374668</p>
+                    <p><i class="bi bi-envelope-fill text-danger"></i>ashoktourntravels@gmail.com</p>
+                </div>
+                <div class="col-lg p-3 text-center">
+                    <i class="bi bi-map text-success h3"></i> &nbsp; <span class="h3 text-light">Popular Destination</span>
+                    <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">Shimla</a>
+                    <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">Ladakh</a>
+                    <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">Manali</a>
+                    <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">Chandigarh</a>
+                </div>
+                <div class="col-lg p-3 text-center">
+                    <i class="bi bi-link-45deg text-secondary h3"></i> &nbsp; <span class="h3 text-light">Quick Links</span>
+                    <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">Home</a>
+                    <!-- <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="./package.php">Tour Packages</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">One-Way Taxi</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="./about.php">About</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="./contact.html">Contact Us</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">gallery</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">review</a> -->
+                </div>
+                <div class="col-lg p-3 text-center">
+                    <i class="bi bi-person-check h3"></i> &nbsp; <span class="h3 text-light">Follow Us</span>
+                    <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="https://www.facebook.com/Ashok-Tour-Travels-105122087549206/"><i class="bi bi-facebook text-primary"></i> Facebook</a>
+                    <p class="lead text-center py-1"><a href="./pages/giveReview.php" class="btn btn-outline-warning btn-small">Leave your review here </a> </p>
+                    <!-- <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">instagram</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">twitter</a>
+                <a class="nav-link text-white border-top px-3 py-1 border-secondary" href="#">linkedin</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="container-flex mx-0 lead px-3 py-1">
+            <p class="credit lead border-top border-warning px-5 py-1">
+                Created by <a href="https://nirojpoudel.com.np" target="_blank" class="text-decoration-none text-warning">npoudelp</a> &nbsp; | &nbsp; All Rights Reserved &copy; <?php echo date('Y'); ?>
+            </p>
+        </div>
+    </section>
 </body>
 
 </html>
